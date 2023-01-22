14424b335581fb7e8268a97fb8dc593eb91c2f4d
Crevan <a.siverskii@gmail.com>
2023-01-22 20:18:32 +0100

    [add] commands.js

---
+++ js/commands.js
@@ -0,0 +1,53 @@
+const linkedin = "https://www.linkedin.com/in/asiverskii/";
+const github = "https://github.com/Craevan/";
+const email = 'mailto:a.siverskii@gmail.com';
+
+whois = [
+    "<br>",
+    "WHOIS TEXT",
+    "<br>"
+];
+
+whoami = [
+    "<br>",
+    "WHOAMI TEXT",
+    "<br>"
+];
+
+social = [
+    "<br>",
+    'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/asiverskii' + "</a>",
+    'github         <a href="' + github + '" target="_blank">github/Craevan' + "</a>",
+    "<br>"
+];
+
+projects = [
+    "<br>",
+    "PROJECTS TEXT",
+    "<br>"
+];
+
+help = [
+    "<br>",
+    '<span class="command">whois</span>          Who is Craevan?',
+    '<span class="command">whoami</span>         Who are you?',
+    '<span class="command">social</span>         Display social networks',
+    '<span class="command">projects</span>       View coding projects',
+    '<span class="command">history</span>        View command history',
+    '<span class="command">help</span>           You obviously already know what this does',
+    '<span class="command">email</span>          Do not email me',
+    '<span class="command">clear</span>          Clear terminal',
+    '<span class="command">banner</span>         Display the header',
+    "<br>",
+];
+
+banner = [
+    "                                        _ __  __        __      _    ",
+    "  __________ ____ _  _____  ___   ___ _(_) /_/ /  __ __/ /     (_)__ ",
+    "/ __/ __/ _ `/ -_) |/ / _ `/ _ \\_/ _ `/ / __/ _ \\/ // / _ \\_  / / _ \\",
+    "\\__/_/  \\_,_/\\__/|___/\\_,_/_//_(_)_, /_/\\__/_//_/\\_,_/_.__(_)/_/\\___/",
+    "                                /___/                               \n",
+    '<span class="color2">Welcome to my interactive web terminal.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> " +
+    "<span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
